1875cddabfe9de3d5366ca9b1a8f8711a01b7484
bilguunbattulga737-ui <bilguunbattulga737@gmail.com>
2026-08-02 22:26:42 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from './app/src/app/App.tsx';
-import './default_shadcn_theme.css';
+import '../default_shadcn_theme.css';
 
 createRoot(document.getElementById("root")!).render(<App />);
