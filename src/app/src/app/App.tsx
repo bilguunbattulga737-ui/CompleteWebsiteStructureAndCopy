0ca73ce0f163e67f5d5b4f5b2ad0e1792a7308e6
bilguunbattulga737-ui <bilguunbattulga737@gmail.com>
2026-08-02 23:45:09 +0800

    Update App.tsx

--- src/app/src/app/App.tsx
+++ src/app/src/app/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import { LanguageProvider } from "./context/LanguageContext";
 
